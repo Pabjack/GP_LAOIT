64c8b30220a924e4ba536b8813ba7aa0599a6d1d
MiguelEsc <20030077@itcelaya.edu.mx>
2024-03-11 16:44:21 -0600

    prueba de front end

---
+++ example-app/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/404', function () {
+    return view('404');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/courses', function () {
+    return view('courses');
+});
+
+Route::get('/team', function () {
+    return view('team');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
