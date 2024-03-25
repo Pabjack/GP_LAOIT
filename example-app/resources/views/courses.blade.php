70d83471078af2d2e6d2d3e8a1807f735046ca3c
miguelesc1409 <127141525+miguelesc1409@users.noreply.github.com>
2024-03-25 17:14:05 -0600

    cursos actualizado

--- example-app/resources/views/courses.blade.php
+++ example-app/resources/views/courses.blade.php
@@ -1,6 +1,48 @@
 @extends('layout/template')
-@section('name','courses')
+@section('name','Cursos')
 @section('content')
+
+    <!-- Carrussel -->
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/logo.png" class=" d-block mx-auto" style="height: 50%" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="coltcarr">First slide label</h5>
+              <p class="colcarr">Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/team-1.jpg" class="d-block mx-auto" style="height: 50%"  alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="coltcarr">Second slide label</h5>
+              <p class="colcarr">Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/team-2.jpg" class="d-block mx-auto" style="height: 50%"  alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="coltcarr">Third slide label</h5>
+              <p class="colcarr">Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
 
     <!-- Categories Start -->
     <div class="container-xxl py-5 category">
